d1a809c7efb86dc3c53b347436531a179b90f131
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2024-10-14 13:22:14 +0530

    update filters for processing

--- index.js
+++ index.js
@@ -11,9 +11,27 @@ if (!fs.existsSync(audioSaveFolder)) {
   fs.mkdirSync(audioSaveFolder); // Create the folder if it doesn't exist
 }
 
+async function processTar(filePath) {
+  await tar.x({
+    file: filePath,
+    cwd: audioSaveFolder, // Extract to the audio folder
+    filter: (path) => {
+      return (
+        path.endsWith("_AUDIO.wav") ||
+        path.endsWith("_Transcript.csv") ||
+        path.includes("openSMILE") ||
+        path.includes("densenet201") ||
+        path.includes("vgg16")
+      );
+    },
+  });
+}
+
 async function processEntry(entry, index) {
   try {
     console.log(`Processing entry ${index + 1}: ${entry}`);
+
+    const entryNo = entry.split("/").at(-1).split("_").at(0);
 
     // Download the .tar.gz file
     const response = await axios({
@@ -22,22 +40,32 @@ async function processEntry(entry, index) {
       responseType: "stream",
     });
 
+    // Get the total length of the file for progress calculation
+    const totalLength = response.headers["content-length"];
+
+    let downloadedLength = 0;
+
     // Save the downloaded file
     const writer = fs.createWriteStream(tempLocation);
     response.data.pipe(writer);
+
+    response.data.on("data", (chunk) => {
+      downloadedLength += chunk.length;
+
+      // Calculate and print progress
+      const percentage = ((downloadedLength / totalLength) * 100).toFixed(2);
+      process.stdout.write(`Downloading Entry ${index + 1}: ${percentage}%\r`);
+    });
+    // Save the downloaded file
 
     await new Promise((resolve, reject) => {
       writer.on("finish", resolve);
       writer.on("error", reject);
     });
 
-    // Extract audio files (e.g., .wav) from the tarball
-    await tar.x({
-      file: tempLocation,
-      cwd: audioSaveFolder, // Extract to the audio folder
-      filter: (path) => path.endsWith(".wav"), // Only extract audio files
-    });
+    await processTar(tempLocation);
 
+    // Extract audio files (e.g., .wav) from the tarball
     console.log(`Extracted audio for entry ${index + 1}`);
   } catch (err) {
     console.error(`Error processing entry ${index + 1}:`, err.toString());
@@ -52,6 +80,13 @@ async function processEntry(entry, index) {
     });
   }
 }
+
+// const pathToTar = "./300_P.tar.gz";
+//
+// processTar(pathToTar).then(() => {
+//   process.exit(0);
+// });
+//
 
 async function processAllEntries() {
   for (let i = 0; i < entries.length; i++) {
